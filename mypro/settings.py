6005a2e416976c58ff13bcac146ae4f76c2becb9
dhanushge41-svg <dhanush.ge41@gmail.com>
2026-05-22 15:23:08 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -69,9 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mypro.wsgi.application'
-STATICFILES_DIRS =[
-    BASE_DIR/'static'
-]
+
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
@@ -118,5 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_DIRS =[
+    BASE_DIR/'static'
+]
