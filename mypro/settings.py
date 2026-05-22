936298f9b070ba18af07717de1e2e6b51d9df3d6
dhanushge41-svg <dhanush.ge41@gmail.com>
2026-05-22 17:59:34 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
   
 ]
 
@@ -121,3 +122,4 @@ STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_DIRS =[
     BASE_DIR/'static'
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
